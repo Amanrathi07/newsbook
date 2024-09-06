bd9ab00be782dfb2bfdcad5fde4ab9b15f2feb72
Amanrathi07 <amanrathi4321@gmail.com>
2024-09-06 05:55:46 +0530

    desine the site

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar  title="newsbook"/>
+        <Navbar  title="newsBook"/>
         <Newsbox />
       </div>
     )
--- src/container/Navbar.js
+++ src/container/Navbar.js
@@ -6,9 +6,9 @@ export default class Navbar extends Component {
     let {title}=this.props ;
     return (
       <>
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg" style={{backgroundColor:"#eaf1ea",}}>
         <div className="container-fluid">
-            <a className="navbar-brand" href="/">{title}</a>
+            <a className="navbar-brand" href="/"><strong>{title}</strong></a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
