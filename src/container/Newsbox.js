90deabea1049b1c270869810efde336686b681ad
Amanrathi07 <amanrathi4321@gmail.com>
2024-09-13 19:01:56 +0530

    make button usable

--- src/container/Newsbox.js
+++ src/container/Newsbox.js
@@ -9,29 +9,52 @@ export default class newsbox extends Component {
 
     this.state = {
       articles: this.articles,
-
+      page:1,
+      
     }
 
   }
 
   async componentDidMount(){
-    let url="https://newsapi.org/v2/top-headlines?country=us&apiKey=e34bd0d6fd01488c80f93871d69b649f&page=1&pageSize=6";
-
-    
+    let url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=e34bd0d6fd01488c80f93871d69b649f&page=${this.state.page}&pageSize=6`;    
 
     let data=await fetch(url)
     
     let newsData=await data.json() ;
 
-    this.setState({articles:newsData.articles})
+    this.setState({articles:newsData.articles,totalResults:newsData.totalResults})
   }
 
-  hendelNextClick=()=>{
+  hendelNextClick=async()=>{
+   
+  if(this.state.page+1 > Math.ceil(this.state.totalResults/6)){}
+  else{
     console.log("next")
+    let url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=e34bd0d6fd01488c80f93871d69b649f&page=${this.state.page + 1}&pageSize=6`;    
+
+    let data=await fetch(url)
+    
+    let newsData=await data.json() ;
+    this.setState({
+      page:this.state.page+1,
+      articles:newsData.articles
+    })
+  }
+    
   }
 
-  hendelPervClick=()=>{
+  hendelPervClick=async()=>{
     console.log("perverse")
+    
+    let url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=e34bd0d6fd01488c80f93871d69b649f&page=${this.state.page - 1}&pageSize=6`;    
+
+    let data=await fetch(url)
+    
+    let newsData=await data.json() ;
+    this.setState({
+      page:this.state.page-1,
+      articles:newsData.articles
+    })
   }
 
 
@@ -53,8 +76,8 @@ export default class newsbox extends Component {
         </div>
 
       <div className="my-3 d-flex justify-content-between">
-      <button type="button" disabled={false} onClick={this.hendelPervClick} className="btn btn-dark">&larr;perverse</button>
-      <button type="button" onClick={this.hendelNextClick} className="btn btn-dark">Next&rarr;</button>
+      <button type="button" disabled={this.state.page<=1} onClick={this.hendelPervClick} className="btn btn-dark">&larr;perverse</button>
+      <button type="button" disabled={this.state.page+1 > Math.ceil(this.state.totalResults/6)} onClick={this.hendelNextClick} className="btn btn-dark">Next&rarr;</button>
       </div>    
 
 
